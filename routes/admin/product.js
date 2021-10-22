91df915f40d243d7457c6bd938b9651721f0922e
KrishnadasPC <pckrishnadas88@gmail.com>
2021-10-22 19:38:56 +0530

    product list admin panel WIP.

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -6,6 +6,12 @@ var categoryModel = require('../../models/category')
 // const upload = multer({ dest: 'images/products' })
 const validator = require('../../validations/validator')
 
+router.get('/', async function(req, res, next) {
+    const products = await productModel.getProducts(10)
+    
+    res.render("admin/products/index.html", {products: products})
+});
+
 router.get('/create', async function(req, res, next) {
     const categories = await categoryModel.getCategories()
     
@@ -14,8 +20,10 @@ router.get('/create', async function(req, res, next) {
 router.post('/create', async function(req, res, next) {
     
     let {title, description, price, quantity, category_id} = req.body;
-    description = description.trim()
-    console.log("title is", typeof title)
+    title = title.trim()
+    description = description.trim()   
+    price = price.trim()   
+    quantity = quantity.trim()   
     let errorBag = {
         title:[],
         description: [],
@@ -34,7 +42,7 @@ router.post('/create', async function(req, res, next) {
     }
 
     const descriptionRequired = validator.required(description, "Description")
-    const descriptionMinLength = validator.minLength(description, 25, "Description")
+    const descriptionMinLength = validator.minLength(description, 10, "Description")
    
     if(typeof descriptionRequired === 'object' && descriptionRequired.constructor === Object){
         errorBag.description.push(descriptionRequired)
@@ -43,24 +51,45 @@ router.post('/create', async function(req, res, next) {
     if(typeof descriptionMinLength === 'object' && descriptionMinLength.constructor === Object){
         errorBag.description.push(descriptionMinLength)
     }
-    if(errorBag.title != [] || errorBag.description != []){
+    const priceRequired = validator.required(price, "Price")
+    if(typeof priceRequired === 'object'){
+        errorBag.price.push(priceRequired)
+    }
+    const pricePostiveNumber = validator.isPositiveNumber(price, "Price")
+    if(typeof pricePostiveNumber === 'object' && pricePostiveNumber.constructor === Object ){
+        errorBag.price.push(pricePostiveNumber)
+    }
+
+    const quantityRequired = validator.required(quantity, "Quantity")
+    if(typeof quantityRequired === 'object' && quantityRequired.constructor === Object ){
+        errorBag.quantity.push(quantityRequired)
+    }
+
+    const quantityPostiveNumber = validator.isPositiveInteger(quantity, "Quantity")
+    if(typeof quantityPostiveNumber === 'object' && quantityPostiveNumber.constructor === Object ){
+        errorBag.quantity.push(quantityPostiveNumber)
+    }
+    
+    const product = {
+        title : title,
+        description: description,
+        price: price,
+        quantity: quantity,
+        category_id: category_id,
+    } 
+    if(errorBag.title.length > 0 || errorBag.description.length > 0|| errorBag.price.length > 0 || errorBag.quantity.length > 0){
         console.log(errorBag)
+        console.log("inside error block")
         const categories = await categoryModel.getCategories()
         res.render("admin/products/create.html", {
-            form: req.body, 
+            form: product, 
             categories: categories,
             errorBag: errorBag
         })
         return;
     }
-    // TODO: validations 
-    const product = {
-        title : req.body.title,
-        description: req.body.description,
-        price: req.body.price,
-        quantity: req.body.quantity,
-        category_id: req.body.category_id,
-    }    
+   
+       
     let productCreated = await productModel.createProduct(product);   
     console.log(productCreated.affectedRows)
     console.log(productCreated)
--- validations/validator.js
+++ validations/validator.js
@@ -19,9 +19,32 @@ function required(item, name) {
         message:`${name} is required.`
     }
 }
+function isPositiveNumber(item, name) {    
+    if(item > 0)
+    {
+        return true;
+    }
+    return { 
+        field: name,
+        message:`${name} must be numeric greater than 0.`
+    }
+}
+function isPositiveInteger(item, name) {   
+    
+    if(item % 1 === 0 && item > 0)
+    {
+        return true;
+    }
+    return { 
+        field: name,
+        message:`${name} must be an integer greater than 0.`
+    }
+}
 
 
 module.exports = {
     minLength: minLength,
     required: required,
+    isPositiveNumber: isPositiveNumber,
+    isPositiveInteger: isPositiveInteger,
 }
